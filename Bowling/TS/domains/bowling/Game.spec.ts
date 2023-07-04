c5c4a12f14401cc170cecf56f63b249e903b4e8a
Vngard <thomas.bonnecaze@protonmail.com>
2023-07-04 10:31:39 +0200

    add gitignore and exclude node modules

---
+++ Bowling/TS/domains/bowling/Game.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import Game from "./Game";
+
+export default function() {
+
+    describe('Start a simple game', function() {
+        it('should start a bowling game', async function() {
+            const game = new Game();
+            expect
+        });
+    });
+}
---
+++ Bowling/TS/test/mocha.spec.ts
@@ -0,0 +1,7 @@
+import GameSpec from "../domains/bowling/Game.spec";
+import { Suite } from "mocha";
+
+describe('Server unit testing', function(this: Suite) {
+    describe('Game', GameSpec);
+}
+);
